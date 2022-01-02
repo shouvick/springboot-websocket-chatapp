0b754c2fa8e0706a38d73202eb083bb1a604c452
unknown <shohagmonzur313@gmail.com>
2022-01-02 15:54:19 +0600

    public chat added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Boot Chat App WebSocket
+
+# Features
+- Group Chat
+
+### Instruction
+
+1. git clone https://github.com/shouvick/springboot-websocket-chatapp.git
+2. Run the project by command ./mvnw spring-boot:run
---
+++ src/test/java/com/springboot/websocketchatapp/WebsocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.websocketchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
